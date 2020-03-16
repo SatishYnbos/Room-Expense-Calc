82414fa8db976a4ab87f52e8be6e85af24aaae11
Satish Yadlapalli <Satish_Yadlapalli@epam.com>
2020-03-16 22:19:44 +0530

    initial commit

---
+++ room-calc-portal/room-calc-api/src/main/java/com/room/calc/RoomCalcApplication.java
@@ -0,0 +1,13 @@
+package com.room.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomCalcApplication.class, args);
+	}
+
+}
